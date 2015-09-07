235de981d24bf5f840189ec0a65c0f5da650a3e1
Javi <jmesamarrero@gmail.com>
2015-09-07 08:31:19 +0100

    update index.html

---
+++ untitled.js
@@ -0,0 +1,12 @@
+var solution1 = function(input){
+    var sum = 0;
+    for(var i = 1; i < input; i++){
+        if(i % 3 === 0){
+            sum += i;
+        }
+        else if(i % 5 === 0){
+            sum += i;
+        }
+    }
+    return sum;
+};
